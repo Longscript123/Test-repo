90bc752db84a8d477c3a76d2b385267d050d5fe8
Abhishek <achandr1@andrew.cmu.edu>
2020-07-27 01:00:44 -0400

    classes Polymorphism

--- Classes/Classes/src/classes.java
+++ Classes/Classes/src/classes.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 class Product{
 	
@@ -13,10 +14,11 @@ class Product{
 	
 	//Methods
 	void setProductDetails(int pid, String name, int price) {
-		this.pid = pid;
-		this.name = name;
+		this.pid = pid; //this means reference to current object 
+		this.name = name; //LHS belongs to object RHS belongs to method
 		this.price = price;
 	}
+	
 	
 	void showProductDetails() {
 		System.out.println(" Product ID:" +pid);
@@ -26,6 +28,50 @@ class Product{
 	}
 }
 
+class Mobile extends Product{ //Is-a relation, mobile is child product is a parent
+
+	String os;
+	int ram;
+	int sdCardSize;
+	
+	//Constructor
+	Mobile(){
+		System.out.println("Mobile object constructed");
+	}
+	
+	//Methods
+	
+	//Method Overloading
+	//redefined the same method in parent with different input in child
+	//This is a compile time or static polymorphism
+	
+	void setProductDetails(int pid, String name, int price, String os, int ram, int sdCardSize) { 
+		this.pid = pid; 
+		this.name = name; 
+		this.price = price;
+		this.os = os;
+		this.ram = ram;
+		this.sdCardSize = sdCardSize;
+		System.out.println("Data in Product Object");
+	}
+	
+	
+	//Method overriding
+	//Same method name with same inputs in a parent child relation has different outputs
+	//This is a run time or dynamic polymorphism
+	
+	void showProductDetails() {
+		System.out.println(" Product ID:" +pid);
+		System.out.println(" Name:" +name);
+		System.out.println(" Price:" +price);
+		System.out.println(" OS:" +os);
+		System.out.println(" RAM:" +ram);
+		System.out.println(" SD Card:" +sdCardSize);
+
+	}
+}
+
+
 public class classes {
 
 	public static void main(String[] args) {
@@ -33,7 +79,29 @@ public class classes {
 		Product product = new Product();
 		System.out.println("product is   " +product);
 		
-		product.setProductDetails(101, name, price);
+		//Writing data in object
+		product.setProductDetails(101, "Light bulb", 220);
+		
+		//Getting user input
+		Scanner stuff = new Scanner(System.in);
+		
+		System.out.print("Enter a number:");
+		int number = stuff.nextInt();
+		
+		//Printing user input
+		System.out.println("The entered number is:"+ number);
+		
+		//Print product details
+		product.showProductDetails();
+		
+		Mobile mobile = new Mobile(); //Product object gets constructed before the mobile object -Rule of Inheritence
+		
+		//Set mobile object
+		mobile.setProductDetails(128, "Macbook", 23, "Mac OS", 4, 6);
+		mobile.showProductDetails();
+		
+		stuff.close();
+		
 	}
 
 }
